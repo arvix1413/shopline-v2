594c7b4f6a278eb41693a348d2fe1bf6a94ac320
wang Leo <>
2026-03-18 10:00:34 +0800

    fix: add shoplineapp.com to image domains in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
-    unoptimized: true
+    domains: [
+      'images.unsplash.com',
+      'img.shoplineapp.com',
+      'shoplineimg.com',
+      'd31xv78q8gnfco.cloudfront.net',
+    ],
+    unoptimized: true,
   },
 }
 
